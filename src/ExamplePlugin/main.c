9e20ddebaf766aab213c1ac1a4f993c83c50a081
Glummy <58010838+Glumboi@users.noreply.github.com>
2025-08-10 20:17:20 +0200

    Update main.c

--- src/ExamplePlugin/main.c
+++ src/ExamplePlugin/main.c
@@ -8,25 +8,15 @@ GlumityV2DumperExports dumperExports;
 
 __declspec(dllexport) void GlumityMain(HMODULE glumityInternal)
 {
-    GlumityV2Exports_Init(&exports, glumityInternal);
-    GlumityPluginLoader *loader = exports.GetLoaderInstance();
-    if (loader)
-    {
-        GlumityPlugin_printf("Found loader\nTotal plugins loaded: %d\n", "Example Plugin", loader->pluginsCount);
-    }
-
     GlumityV2DumperExports_Init(&dumperExports);
     if (dumperExports.GlumityV2Dumper_GetFunctionPointer)
     {
         GlumityPlugin_printf("Found GlumityV2Dumper_GetFunctionPointer!\n", "Example Plugin");
     }
-
-    void *ptr = dumperExports.GlumityV2Dumper_GetFunctionPointer("BiosConfig", "ChangeCPUMultiplier");
-    if (ptr)
+    void *ChangeCPUMultiplierPtr = dumperExports.GlumityV2Dumper_GetFunctionPointer("BiosConfig", "ChangeCPUMultiplier");
+    if (ChangeCPUMultiplierPtr)
     {
-
         // Make a hook
-        GlumityPlugin_printf("ChangeCPUMultiplier!\n", "Example Plugin");
-        Hooks_Install(ptr);
+        Hooks_Install(ChangeCPUMultiplierPtr);
     }
 }
